3c8587f0eb2c73d998fbf61b604a44cfb15ff33d
fmlizarraga <lizarragafranco91@gmail.com>
2025-04-14 11:58:29 -0300

    todo list and item styles

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,9 +1,22 @@
+import { FaCheckCircle, FaEllipsisV } from 'react-icons/fa';
+import '../styles/todo-item.css';
+
 export const TodoItem = () => {
     return (
         <div className="todo-item">
             <div className="todo-label"><span>Item 1</span></div>
-            <div className="todo-check"><button>Check</button></div>
-            <div className="todo-options"><button>Options</button></div>
+            <div className="todo-toolbar">
+                <div className="todo-check">
+                    <button className="todo-button">
+                        <FaCheckCircle size={18} />
+                    </button>
+                </div>
+                <div className="todo-options">
+                    <button className="todo-button">
+                        <FaEllipsisV size={18} />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,8 +1,10 @@
 import { TodoItem } from "./TodoItem";
+import '../styles/todo-list.css';
 
 export const TodoList = () => {
     return (
         <div className="todo-list">
+            <TodoItem />
             <TodoItem />
         </div>
     );
